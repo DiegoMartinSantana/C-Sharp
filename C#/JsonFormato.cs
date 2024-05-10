f901255dbe93e603faed93b8e58fec44509ecfc8
Diego Martin Santana <141792703+DiegoMartinSantana@users.noreply.github.com>
2024-05-10 13:09:39 -0300

    Json, Interfaces, Generics, Datos anonimos

---
+++ C#/JsonFormato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CursoCSharp
+{
+     public class JsonFormato
+    {
+        public class Beer
+        {
+            public Beer()
+            {
+                Name = "Quilmes";
+                Alcohol = 10.25m;
+            }
+
+            public string Name { get; set; }
+            public decimal Alcohol { get; set; }
+
+        }
+    
+        
+        
+    }
+}
